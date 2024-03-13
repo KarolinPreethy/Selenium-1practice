ba9be78b97d124462296c55dbb13bd56d96badd1
Administrator <Administrator@172.31.14.133>
2024-03-13 08:30:50 +0000

    Add new file

---
+++ demoselenium/src/main/java/demoselenium1/Day1.java
@@ -0,0 +1,35 @@
+package demoselenium1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+	
+
+	
+		public static void main(String[] args) {
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://selenium.dev/selenium/web/web-form.html");
+			String title=driver.getTitle();
+			System.out.println("the title is:"+title);
+			
+			//Enter text in field 
+			WebElement name=driver.findElement(By.id("my-text-id"));
+			// helpful to clear data in html if any
+			name.clear();
+			name.sendKeys("Karolin");
+			
+			//Printing entered text in console
+			String Enteredtext=name.getAttribute("value");
+			System.out.println(Enteredtext);
+			
+			//switch to other tab
+			driver.switchTo().newWindow(WindowType.TAB);
+		}
+
+	}
+
+
